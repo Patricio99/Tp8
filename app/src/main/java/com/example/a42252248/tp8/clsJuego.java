b31a30ca97b069792353183be76016b8c7883c8f
Patricio99 <patonaveira@gmail.com>
2018-02-15 09:52:31 -0300

    es una caca

--- app/src/main/java/com/example/a42252248/tp8/clsJuego.java
+++ app/src/main/java/com/example/a42252248/tp8/clsJuego.java
@@ -1,10 +1,12 @@
 package com.example.a42252248.tp8;
 
+import org.cocos2d.actions.interval.ScaleBy;
 import org.cocos2d.layers.Layer;
 import org.cocos2d.nodes.Director;
 import org.cocos2d.nodes.Scene;
 import org.cocos2d.nodes.Sprite;
 import org.cocos2d.opengl.CCGLSurfaceView;
+import org.cocos2d.particlesystem.ParticleSpiral;
 import org.cocos2d.types.CCSize;
 
 /**
@@ -15,6 +17,7 @@ public class clsJuego {
     CCGLSurfaceView _VistaDelJuego;
     CCSize DeviceDisplay;
     Sprite AutoJugador;
+    Sprite BackgroundImage;
     public clsJuego(CCGLSurfaceView VistaDelJuego){
         _VistaDelJuego = VistaDelJuego;
     }
@@ -41,6 +44,17 @@ public class clsJuego {
         return EscenaADevolver;
     }
     class CapaDeFondo extends Layer{
+        public CapaDeFondo(){
+            PonerBackground();
+        }
+        private void PonerBackground(){
+            BackgroundImage = Sprite.sprite("background.png");
+            BackgroundImage.setPosition(DeviceDisplay.width/2, DeviceDisplay.height/2);
+
+            BackgroundImage.runAction(ScaleBy.action(0.01f, 2.0f, 2.0f));
+
+            super.addChild(BackgroundImage);
+        }
 
     }
     class CapaDelFrente extends Layer {
@@ -50,7 +64,10 @@ public class clsJuego {
         private void PonerAutoPosInicial(){
             AutoJugador = Sprite.sprite("AutoJugador.png");
 
-            AutoJugador.setPosition(100, 300);
+            float PosIX, PosIY;
+            PosIX = DeviceDisplay.width/2;
+            PosIY = AutoJugador.getHeight()/2;
+            AutoJugador.setPosition(PosIX, PosIY);
 
             super.addChild(AutoJugador);
         }
