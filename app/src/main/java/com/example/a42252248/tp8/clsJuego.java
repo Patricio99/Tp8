d24e1a12ae4b679be004a41d1a7fea56c4af5845
Patricio99 <patonaveira@gmail.com>
2018-02-20 01:59:15 -0300

    Avances
    Manana tengo que ver el tema de las flechitas, izquierda y derecha. Despues hacer que las monedas se vean arriba a la derecha. Agregar un mensaje rojo cuando el player tenga la velocidad maxima. Hacer un layer aparte con un boton para iniciar el juego

--- app/src/main/java/com/example/a42252248/tp8/clsJuego.java
+++ app/src/main/java/com/example/a42252248/tp8/clsJuego.java
@@ -1,5 +1,7 @@
 package com.example.a42252248.tp8;
 
+import android.util.Log;
+
 import org.cocos2d.actions.interval.MoveTo;
 import org.cocos2d.actions.interval.ScaleBy;
 import org.cocos2d.layers.Layer;
@@ -10,27 +12,34 @@ import org.cocos2d.opengl.CCGLSurfaceView;
 import org.cocos2d.particlesystem.ParticleSpiral;
 import org.cocos2d.types.CCPoint;
 import org.cocos2d.types.CCSize;
+import org.w3c.dom.Comment;
 
-/**
- * Created by 42252248 on 15/2/2018.
- */
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 public class clsJuego {
     CCGLSurfaceView _VistaDelJuego;
     CCSize DeviceDisplay;
     Sprite AutoJugador;
     Sprite AutoEnemigo;
+    Sprite AutoEnemigo1;
     Sprite BackgroundImage;
     public clsJuego(CCGLSurfaceView VistaDelJuego){
         _VistaDelJuego = VistaDelJuego;
     }
+
     public void ComenzarJuego(){
         Director.sharedDirector().attachInView(_VistaDelJuego);
 
         DeviceDisplay = Director.sharedDirector().displaySize();
 
         Director.sharedDirector().runWithScene(EscenaDelJuego());
+
     }
+
     private Scene EscenaDelJuego(){
         Scene EscenaADevolver;
         EscenaADevolver = Scene.node();
@@ -40,6 +49,10 @@ public class clsJuego {
 
         CapaDelFrente MiCapaFrente;
         MiCapaFrente = new CapaDelFrente();
+
+
+
+
 
         EscenaADevolver.addChild(MiCapaFondo, -10);
         EscenaADevolver.addChild(MiCapaFrente, 10);
@@ -59,50 +72,102 @@ public class clsJuego {
             BkgHeight = DeviceDisplay.height / BackgroundImage.getHeight();
 
             BackgroundImage.runAction(ScaleBy.action(0.01f, BkgWidth, BkgHeight));
-
             super.addChild(BackgroundImage);
         }
 
     }
+
     class CapaDelFrente extends Layer {
+        private Player player;
+        private ArrayList<AutoEnemigo> autoEnemigosLst = new ArrayList<>();
+        private boolean userLoose = false;
+
         public CapaDelFrente(){
-            PonerAutoPosInicial();
-            PonerAutoEnemigo();
+            player = new Player(this);
+            // this.setPosition(0, 0);
+            super.schedule("AddEnemy", 3.0f);
         }
-        private void PonerAutoPosInicial(){
-            AutoJugador = Sprite.sprite("AutoJugador.png");
 
-            float PosIX, PosIY;
-            PosIX = DeviceDisplay.width/2;
-            PosIY = AutoJugador.getHeight()/2;
-            AutoJugador.setPosition(PosIX, PosIY);
+        public class Player {
+            private Sprite sprite;
+            private float x;
+            private float y;
+            public int points;
 
-            super.addChild(AutoJugador);
+            private Player(Layer l) {
+                int initialCarril = 2;
+                this.sprite = Sprite.sprite("AutoJugador.png");
+                this.y = 0;
+                this.x = DeviceDisplay.width / 3 * initialCarril + this.sprite.getWidth() / 2;
+
+                this.sprite.setPosition(this.x, this.y);
+                l.addChild(this.sprite);
+            }
         }
-        private void PonerAutoEnemigo(){
-            AutoEnemigo = Sprite.sprite("AutoEnemigo.png");
+        public class AutoEnemigo {
+            private Sprite sprite;
+            private float x;
+            private float y;
+            private float speed;
+            private org.cocos2d.actions.base.Action animation;
 
-            Float PosIX, PosIY;
-            Float EnemyHeight;
-            EnemyHeight = AutoEnemigo.getHeight();
-            PosIY = DeviceDisplay.height + EnemyHeight/2;
-            PosIX = DeviceDisplay.width/2;
+            private AutoEnemigo(Layer l, int spriteNumber, float speed, int carril) {
+                this.sprite = Sprite.sprite("AutoEnemigo" + spriteNumber + ".png");
+                this.speed = speed;
+                this.x = DeviceDisplay.width / 3 * carril + this.sprite.getWidth() / 2;
+                this.y = DeviceDisplay.height + this.sprite.getHeight() / 2;
 
-
-            AutoEnemigo.setPosition(PosIX, PosIY);
-
-            Float PosFX, PosFY;
-            PosFX = PosIX;
-            PosFY = -EnemyHeight/2;
-
-            AutoEnemigo.runAction(MoveTo.action(3, PosFX, PosFY));
+                this.sprite.setPosition(this.x, this.y);
+                l.addChild(this.sprite);
 
 
+            }
+            private void startMoving(){
+                this.animation = this.sprite.runAction(MoveTo.action(this.speed, this.x, -1 * (DeviceDisplay.height + this.sprite.getHeight())));
+            }
+        }
+        public void AddEnemy(float timeDiff) {
 
-            super.addChild(AutoEnemigo);
+            int carril = 0 + (int)(Math.random() * ((2- 0) + 1));
+            int spriteNumber = 1 + (int)(Math.random() * ((2- 1) + 1));
+            float speed = getSpeed(this.player);
+            AutoEnemigo autoEnemigo = new AutoEnemigo(this, spriteNumber, speed, carril);
+            autoEnemigo.startMoving();
 
+            autoEnemigosLst.add(autoEnemigo);
+
+            removeFinishedAutoEnemigo(this.autoEnemigosLst);
+            Log.d("APP", "AddEnemy_" + this.autoEnemigosLst.size() + "_" + speed);
+            if (!this.userLoose) {
+                float nextCar = getNextEnemyTimeElapse(this.player);
+                super.schedule("AddEnemy", nextCar );
+            }
+        }
+        public float getSpeed(Player p) {
+            float speed = - 25 / 10 * p.points + 5;
+            if (speed < 1) {
+                speed = 1;
+            }
+            //TODO REMOVE THIS LINE
+            speed = 5;
+            return speed;
+        }
+        public float getNextEnemyTimeElapse(Player p) {
+            float speed = - 25 / 2 * p.points + 3;
+            if (speed < 1) {
+                speed = 1;
+            }
+            return speed;
+        }
+        public void removeFinishedAutoEnemigo(List<AutoEnemigo> autoEnemigoLst) {
+            for (AutoEnemigo element : autoEnemigoLst) {
+                if (element.animation.isDone()) {
+                    autoEnemigoLst.remove(autoEnemigoLst.indexOf(element));
+                    this.player.points++;
+                }
+            }
         }
 
     }
-
 }
+
